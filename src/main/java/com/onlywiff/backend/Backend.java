9d850d91f3db82c4c433bf01cbc7f4134bf7ea45
Presti <dxssucuk@hotmail.com>
2023-07-02 22:08:07 +0200

    Starting the code

---
+++ src/main/java/com/onlywiff/backend/Backend.java
@@ -0,0 +1,13 @@
+package com.onlywiff.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyWiffBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyWiffBackendApplication.class, args);
+	}
+
+}
